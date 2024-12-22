ea9d9fbe98359fa348d7c3e2c04b5d75a78d809e
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-12-22 20:47:35 +0100

    updated the styling of the homepage

--- src/pages/Dashboard/components/Footer.jsx
+++ src/pages/Dashboard/components/Footer.jsx
@@ -1,4 +1,9 @@
 import "../../../assets/styles/footer.css";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaGamepad } from "react-icons/fa";
+import { CiStar } from "react-icons/ci";
+import { SlMagicWand } from "react-icons/sl";
+import { FaTasks } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -6,25 +11,34 @@ const Footer = () => {
       <div className="footer-menu">
         <div className="menu-item">
           <div className="icon task-icon">
+            <FaTasks />
             <span className="badge">5</span>
           </div>
           <span className="menu-label">Task</span>
         </div>
         <div className="menu-item">
-          <div className="icon top-icon"></div>
+          <div className="icon top-icon">
+            <SlMagicWand />
+          </div>
           <span className="menu-label">Top</span>
         </div>
         <div className="menu-item">
-          <div className="icon hed-hog-icon"></div>
+          <div className="icon hed-hog-icon">
+            <CiStar color="#1b1b1b" />
+          </div>
           <span className="menu-label active">Conquest</span>
         </div>
         <div className="menu-item">
-          <div className="icon game-icon"></div>
+          <div className="icon game-icon">
+            <FaGamepad />
+          </div>
           <span className="menu-label">Game</span>
         </div>
         <div className="menu-item">
-          <div className="icon friends-icon"></div>
-          <span className="menu-label">Friends</span>
+          <div className="icon friends-icon">
+            <FaRegUserCircle color="#1b1b1b" />
+          </div>
+          <span className="menu-label">Profile</span>
         </div>
       </div>
     </div>
