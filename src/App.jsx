5570a7adc3bd9d298e716df05b105552b3d11c51
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-12-22 17:30:49 +0100

    refactored the styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 // import frameOne from "./assets/images/frame1.png";
 import welcome from "./assets/images/welcome.png";
@@ -9,32 +8,34 @@ import { PiBirdFill } from "react-icons/pi";
 function App() {
   return (
     <>
-      <div className="app">
-        <header className="header">
-          <span className="note">Qu</span>est Vi
-          <span className="note">st</span>a
+      <div className="app-container">
+        <header className="header-title">
+          <span className="header-highlight">Qu</span>est Vi
+          <span className="header-highlight">st</span>a
         </header>
-        <div className="subHeader">
-          <span>
+
+        <div className="subheader-container">
+          <span className="subheader-user">
             <PiBirdFill />
             Nick Jay
           </span>
-          <span className="span2">
-            <div className="iconContainer">
+          <span className="subheader-balance">
+            <div className="balance-icon-container">
               <FaEthereum color="#fff" />
             </div>
             Ethereum
           </span>
         </div>
-        <div className="welcomeContainer">
-          <div className="imageContainer">
-            <img src={welcome} alt="" className="welcome-img" />
+
+        <div className="welcome-section">
+          <div className="image-wrapper">
+            <img src={welcome} alt="Welcome" className="welcome-image" />
           </div>
-          <p>
+          <p className="welcome-text">
             Welcome to <strong>Quest Vista</strong>! Start mining and earn Quest
             Tokens while exploring the world of crypto rewards.
           </p>
-          <button className="getStarted">
+          <button className="start-button">
             Start Mining <FaArrowRightLong color="#1b1b1b" size={22} />
           </button>
         </div>
